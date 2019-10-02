a12e3d7109299cad9fc301838461a7497797b20a
SIYI LIN <linsiyi7@Gmail.com>
2019-10-01 22:57:53 -0400

    add the day 1 and day 2 to the repository

---
+++ Day 1 Data Types.py
@@ -0,0 +1,16 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+input_int = int(input())
+input_double = float(input())
+input_string = str(input())
+
+# Print the sum of both integer variables on a new line.
+print(i + input_int)
+# Print the sum of the double variables on a new line. Round it to 1 decimal place
+print(round(d + input_double, 1))
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + input_string)
+
---
+++ Day 2 Conditional Statement.py
@@ -0,0 +1,26 @@
+N = int(input())
+
+if N % 2 == 1:
+    print('Weird')
+elif N < 5:
+    print('Not Weird')
+elif N <= 20:
+    print('Weird')
+else:
+    print('Not Weird')
+
+
+
+"""
+Learning Note. 
+Redundant code + logical statement 
+
+if N % 2 == 1:
+    print('Weird')
+elif N > 2 and N < 5:
+    print('Not Weird')
+elif N >= 6 and N <= 20:
+    print('Weird')
+else:
+    print('Not Weird') 
+"""
